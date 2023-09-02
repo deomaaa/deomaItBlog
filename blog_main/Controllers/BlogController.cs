b7e203982352bd4022757d20145074c6e6dc1901
deomaaa <dmdndrv@gmail.com>
2023-09-02 15:58:55 +0300

    добавил сервис MinioClient | запустил сервер хранилища в docker 9000-api 9090-login

--- blog_main/Controllers/BlogController.cs
+++ blog_main/Controllers/BlogController.cs
@@ -1,14 +1,19 @@
 using blog_main.Data;
 using Microsoft.AspNetCore.Mvc;
+using Minio;
+using Minio.AspNetCore;
 
 namespace blog_main.Controllers
 {
     public class BlogController : Controller
     {
         private readonly IPostRepo _repository;
-        public BlogController(IPostRepo repository)
+        private readonly IMinioClient _minioClient;
+
+        public BlogController(IPostRepo repository, IMinioClientFactory minioClientFactory)
         {
             _repository = repository;
+            _minioClient = minioClientFactory.CreateClient();
         }
 
         [HttpGet]
@@ -17,9 +22,16 @@ namespace blog_main.Controllers
             return View();
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Posts()
+        [HttpGet("{controller}/{action}/{bucketName?}")]
+        public async Task<IActionResult> Posts(string bucketName)
         {
+            var getListBucketsTask = await _minioClient.ListBucketsAsync().ConfigureAwait(false);
+
+            // Iterate over the list of buckets.
+            foreach (var bucket in getListBucketsTask.Buckets)
+            {
+                Console.WriteLine(bucket.Name + " " + bucket.CreationDateDateTime);
+            }
             return View();
         }  
     }
